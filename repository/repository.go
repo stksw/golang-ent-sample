00e075db51da262a299fd115ab382702b118994e
stksw <kakiage@gmail.com>
2022-12-08 12:14:03 +0900

    add todo handler, repository

---
+++ repository/repository.go
@@ -0,0 +1,18 @@
+package repository
+
+import (
+	"ent-sample/ent"
+	"log"
+)
+
+var DB *ent.Client
+
+func OpenDB() (*ent.Client, error) {
+	DB, err := ent.Open("mysql", "root:password@tcp(entdb:3306)/ent-sample?parseTime=true")
+	if err != nil {
+		log.Fatalf("failed opening connection to mysql: %v", err)
+		return nil, err
+	}
+
+	return DB, nil
+}
